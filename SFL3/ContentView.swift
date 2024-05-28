b2022a401d83942b728f7adf929fcf9ac303f40e
dacaiguoguo <dacaiguoguo@163.com>
2024-05-28 13:14:36 +0800

    init commit

--- SFL3/ContentView.swift
+++ SFL3/ContentView.swift
@@ -8,25 +8,100 @@
 import SwiftUI
 import CoreData
 
+import Foundation
+
+func readSflWithFile(filePath: String) -> [String]? {
+    let fileManager = FileManager.default
+    if !fileManager.fileExists(atPath: filePath) {
+        return nil
+    }
+    
+    let fileUrl = URL(fileURLWithPath: filePath)
+    do {
+        let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+        return readSflWithData(data: data)
+    } catch {
+        print(error.localizedDescription)
+        return nil
+    }
+}
+
+func readSflWithData(data: Data) -> [String]? {
+    if data.isEmpty {
+        return nil
+    }
+    
+    var recentList: [Any]?
+    do {
+        if let recentListInfo = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [String: Any] {
+            recentList = recentListInfo["items"] as? [Any]
+        }
+    } catch {
+        print("Exception during unarchiving: \(error)")
+        return nil
+    }
+    
+    guard let items = recentList else {
+        return nil
+    }
+    
+    var mutArray = [String]()
+    
+    for item in items {
+        var resolvedUrl: URL?
+        
+        if let dict = item as? [String: Any], let bookmark = dict["Bookmark"] as? Data {
+            do {
+                var isStale = false
+                resolvedUrl = try URL(resolvingBookmarkData: bookmark, options: .withoutUI, relativeTo: nil, bookmarkDataIsStale: &isStale)
+            } catch {
+                print("Error resolving bookmark: \(error)")
+                continue
+            }
+        }
+        
+        if let urlPath = resolvedUrl?.path {
+            mutArray.append(urlPath)
+        }
+    }
+    
+    return mutArray
+}
+
+
+import SwiftUI
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
 
+    @State private var filePaths: [String] = []
+
+    init() {
+        let filePath = Bundle.main.path(forResource: "com.apple.dt.xcode", ofType: "sfl3") ?? ""
+        self._filePaths = State(initialValue: readSflWithFile(filePath: filePath) ?? [])
+    }
+
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                Section(header: Text("Files")) {
+                    ForEach(filePaths, id: \.self) { filePath in
+                        Text(filePath)
                     }
                 }
-                .onDelete(perform: deleteItems)
+                
+                Section(header: Text("Core Data Items")) {
+                    ForEach(items) { item in
+                        NavigationLink(destination: Text("Item at \(item.timestamp!, formatter: itemFormatter)")) {
+                            Text(item.timestamp!, formatter: itemFormatter)
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
             }
             .toolbar {
                 ToolbarItem {
@@ -39,31 +114,25 @@ struct ContentView: View {
         }
     }
 
-    private func addItem() {
+    private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
+            offsets.map { items[$0] }.forEach(viewContext.delete)
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
+    private func addItem() {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
