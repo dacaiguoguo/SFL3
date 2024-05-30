23da96be08b52ea8f7d846d07e7f41a29c7464bb
dacaiguoguo <dacaiguoguo@163.com>
2024-05-30 11:15:09 +0800

    find app icon

--- SFL3/FileWatcher.swift
+++ SFL3/FileWatcher.swift
@@ -92,3 +92,38 @@ class FilePathsViewModel: ObservableObject {
     }
 }
 
+import SwiftUI
+
+class IconFinder: ObservableObject {
+    @Published var icons: [URL] = []
+
+    func findAppIcon(in filePath: String) {
+        DispatchQueue.global(qos: .default).async {
+            let fileManager = FileManager.default
+            let workPathURL = URL(fileURLWithPath: filePath)
+            let enumerator = fileManager.enumerator(at: workPathURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+
+            var appiconsetURL: URL?
+            while let fileURL = enumerator?.nextObject() as? URL {
+                if fileURL.pathExtension == "appiconset" {
+                    appiconsetURL = fileURL
+                    break // Stop after finding the first appiconset folder
+                }
+            }
+
+            var foundIcon = false // Control flag to break the loop
+
+            if let appiconset = appiconsetURL {
+                let iconEnumerator = fileManager.enumerator(at: appiconset, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+                while let iconFileURL = iconEnumerator?.nextObject() as? URL, !foundIcon { // Check the flag
+                    if let _ = try? Data(contentsOf: iconFileURL) {
+                        DispatchQueue.main.async {
+                            self.icons.append(iconFileURL)
+                        }
+                        foundIcon = true // Set the flag to true to break the loop
+                    }
+                }
+            }
+        }
+    }
+}
